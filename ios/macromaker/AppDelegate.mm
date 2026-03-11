919188d250e5cf5b3f5c15a9d2f22dfe11f2715c
Cary Meskell <carymeskell@gmail.com>
2026-03-11 11:54:16 -0700

    feat(02-02): rewrite AppDelegate to RCTAppDelegate pattern
    - AppDelegate.h: inherit from RCTAppDelegate, drop UIWindow property and RCTBridgeDelegate
    - AppDelegate.mm: new Obj-C++ file with moduleName/initialProps, bundleURL method, and customizeRootView bootsplash hook
    - Remove AppDelegate.m (superseded by .mm for New Architecture C++ interop)

---
+++ ios/macromaker/AppDelegate.mm
@@ -0,0 +1,33 @@
+#import "AppDelegate.h"
+#import <React/RCTBundleURLProvider.h>
+#import "RNBootSplash.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"main";
+  self.initialProps = @{};
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+- (void)customizeRootView:(RCTRootView *)rootView
+{
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
+}
+
+@end
