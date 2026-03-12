c233f7e801539edbfdccf24387a1a8e410d12d6e
Cary Meskell <carymeskell@gmail.com>
2026-03-11 20:36:24 -0700

    test(phase-03-01): adapt styled text snapshot for react 19

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -3,8 +3,16 @@ import renderer from 'react-test-renderer';
 
 import { MonoText } from '../StyledText';
 
+jest.mock('../../hooks/useColorScheme', () => () => 'light');
+
 it(`renders correctly`, () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  let testRenderer;
+
+  renderer.act(() => {
+    testRenderer = renderer.create(<MonoText>Snapshot test!</MonoText>);
+  });
+
+  const tree = testRenderer.toJSON();
 
   expect(tree).toMatchSnapshot();
 });
